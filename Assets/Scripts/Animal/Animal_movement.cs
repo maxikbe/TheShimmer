7548166afbcf64244ae9787a90cd5bc79926abbd
Dravlix <ominikd@seznam.cz>
2026-02-03 21:31:33 +0100

    Pridan switch pro animace (zakomentovany aby nespamoval). Pro přidání běh/chůze přidat vypočítání rychlosti u hmotné postavy ne ducha (to tam zatím není)

--- Assets/Scripts/Animal/Animal_movement.cs
+++ Assets/Scripts/Animal/Animal_movement.cs
@@ -34,6 +34,9 @@ public class Animal_movement : MonoBehaviour
 
     // Reference na vytvořeného ducha, abychom ho mohli sledovat
     private Ghost_movement myGhost;
+    
+    private Vector3 lastPosition;
+    private const float MovementTreshold = 0.005f; //citlivost
 
     void Start()
     {
@@ -64,11 +67,66 @@ public class Animal_movement : MonoBehaviour
     {
         if (myGhost != null)
         {
+            Vector3 previousPos = transform.position;
             // Tady řešíš vizuální pohyb za duchem (Lerp), viz předchozí rada
             transform.position = Vector3.Lerp(transform.position, myGhost.transform.position, Time.deltaTime * 5f);
             
+            UpdateAnimationDirection(transform.position - previousPos);
             // Tady bys mohl řešit animace podle myGhost.agent.velocity atd.
         }
+    }
+    
+    // --- NOVÁ METODA NA SMĚRY ---
+    void UpdateAnimationDirection(Vector3 movementVector)
+    {
+        // Pokud je pohyb moc malý, považujeme to za IDLE a neřešíme směr
+        if (movementVector.magnitude < MovementTreshold)
+        {
+            // Debug.Log("IDLE (Stojím)");
+            return;
+        }
+
+        // Normalizujeme vektor (chceme jen směr, ne délku)
+        Vector3 dir = movementVector.normalized;
+
+        // vraci v radialech prevadime na stupne
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        if (angle < 0) angle += 360;
+
+        //podle stupnu
+        float step = 360f / 8f;
+        int sector = Mathf.FloorToInt((angle + step / 2) % 360 / step);
+        //---------------------------- ZAKOMENTOVANY SWITCH ABY TO NESPAMOBALO-------------------------------------------------"""""""!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        /*
+        switch (sector)
+        {
+            case 0:
+                Debug.Log("RIGHT (Východ) ➡");
+                break;
+            case 1:
+                Debug.Log("UP-RIGHT (Severovýchod) ↗");
+                break;
+            case 2:
+                Debug.Log("UP (Sever) ⬆");
+                break;
+            case 3:
+                Debug.Log("UP-LEFT (Severozápad) ↖");
+                break;
+            case 4:
+                Debug.Log("LEFT (Západ) ⬅");
+                break;
+            case 5:
+                Debug.Log("DOWN-LEFT (Jihozápad) ↙");
+                break;
+            case 6:
+                Debug.Log("DOWN (Jih) ⬇");
+                break;
+            case 7:
+                Debug.Log("DOWN-RIGHT (Jihovýchod) ↘");
+                break;
+        }
+        */
     }
     
     // Uklidíme po sobě
