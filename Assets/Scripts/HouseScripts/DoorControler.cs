844f43968c59033266c2351900871db9b2efd293
maxikbe <belunekmax@gmail.com>
2026-04-09 09:41:33 +0200

    Commit --- more rychle save mam 5% aaa ----- interier, transitons, popupui, ui, spawnTonw logic , etc.. nestiham

---
+++ Assets/Scripts/HouseScripts/DoorControler.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
+
+public class DoorControler : MonoBehaviour
+{
+    public string sceneToLoad;
+    public bool isFunctional = false;
+    public string spawnPointName;
+    public string popUpTextContent = "It´s blocked...";
+    public float transitionWaitTime = 1f;
+
+    private GameObject InteractUI;
+    private GameObject PopUpUI;
+    private TextMeshProUGUI popUpText;
+    private Animator popUpAnimator;
+    private Animator transitionAnimator;
+    private GameObject TransitionPanel;
+
+    private bool isPlayerInTrigger = false;
+
+    private void Update()
+    {
+        if (isPlayerInTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            if (isFunctional)
+            {
+                StartCoroutine(ProcessMove());
+            }
+            else
+            {
+                if (PopUpUI != null && popUpText != null && popUpAnimator != null)
+                {
+                    popUpText.text = popUpTextContent;
+                    PopUpUI.SetActive(true);
+                    popUpAnimator.Play("PopUpUI", -1, 0f);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            InteractUI = other.transform.Find("PlayerInfoUICanvas/Interaction").gameObject;
+
+            if (!isFunctional)
+            {
+                PopUpUI = other.transform.Find("PlayerInfoUICanvas/PopUp").gameObject;
+                popUpText = PopUpUI.GetComponentInChildren<TextMeshProUGUI>();
+                popUpAnimator = PopUpUI.GetComponent<Animator>();
+            }
+            else
+            {
+                TransitionPanel = other.transform.Find("PlayerInfoUICanvas/TransitionPanel").gameObject;
+                transitionAnimator = TransitionPanel.GetComponent<Animator>();
+            }
+            
+            isPlayerInTrigger = true;
+            if (InteractUI != null) InteractUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+            if (InteractUI != null) InteractUI.SetActive(false);
+            if (PopUpUI != null) PopUpUI.SetActive(false); 
+        }
+    }
+
+    IEnumerator ProcessMove()
+    {
+        if (InteractUI != null) InteractUI.SetActive(false);
+
+        if (TransitionPanel != null && transitionAnimator != null)
+        {
+            TransitionPanel.SetActive(true);
+            transitionAnimator.Play("TransitionAnimationReversed", -1, 0f); 
+        }
+
+        yield return new WaitForSeconds(transitionWaitTime);
+
+        PlayerPrefs.SetString("LastSpawnPoint", spawnPointName);
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
---
+++ Assets/Scripts/HouseScripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public float fadeOutDelay = 1f;
+
+    void Start()
+    {
+        GameObject canvas = GameObject.Find("PlayerInfoUICanvas");
+        if (canvas != null)
+        {
+            Transform panelTransform = canvas.transform.Find("TransitionPanel");
+            if (panelTransform != null)
+            {
+                GameObject transitionPanel = panelTransform.gameObject;
+                Animator transitionAnimator = transitionPanel.GetComponent<Animator>();
+
+                if (transitionAnimator != null)
+                {
+                    transitionPanel.SetActive(true);
+                    transitionAnimator.Play("TransitionAnimation", -1, 0f);
+                    Debug.Log(123);
+                }
+            }
+        }
+
+        if (PlayerPrefs.HasKey("LastSpawnPoint"))
+        {
+            string targetName = PlayerPrefs.GetString("LastSpawnPoint");
+            GameObject spawnPoint = GameObject.Find(targetName);
+
+            if (spawnPoint != null)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                if (player != null)
+                {
+                    player.transform.position = spawnPoint.transform.position;
+                }
+            }
+
+            PlayerPrefs.DeleteKey("LastSpawnPoint");
+        }
+    }
+}
