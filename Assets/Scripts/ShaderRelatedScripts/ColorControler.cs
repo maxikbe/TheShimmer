6aeeffc1417996067f0b970e006ef4fac270b8f2
maxikbe <belunekmax@gmail.com>
2026-04-10 07:23:24 +0200

    Commit -save

--- Assets/Scripts/ShaderRelatedScripts/ColorControler.cs
+++ Assets/Scripts/ShaderRelatedScripts/ColorControler.cs
@@ -6,7 +6,7 @@ public class IndividualTreeLogic : MonoBehaviour
     [Header("Color Settings")]
     public Color newColor = Color.blue; 
     [Range(0f, 1f)]
-    public float transparency = 1.0f; // 1 is solid, 0 is clear
+    public float transparency = 1.0f;
 
     [Header("Detection Settings")]
     public Color colorToReplace = new Color(0.1f, 0.5f, 0.1f);
@@ -26,7 +26,6 @@ public class IndividualTreeLogic : MonoBehaviour
 
         spriteRenderer.GetPropertyBlock(propBlock);
 
-        // We pass the newColor but override its Alpha with our slider
         Color finalColor = newColor;
         finalColor.a = transparency;
 
